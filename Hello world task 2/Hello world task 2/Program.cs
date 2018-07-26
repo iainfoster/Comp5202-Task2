fe72e565b2c35266e7e4cca276a43426c8d7ea54
Iain Foster <9925082@student.toiohomai.ac.nz>
2018-07-26 14:19:18 +1200

    task1

---
+++ Hello world task 2/Hello world task 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "Hello World";
+            Console.WriteLine(output);
+
+            Console.ReadKey();
+
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
